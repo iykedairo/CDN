487be7ed450f551c1f0b4d2068a3eee4f55a0d5f
michaeliyke <ymichaelc@gmail.com>
2017-08-23 23:22:49 +0100

    Techie Inc. Autos:- Wed 08/23/2017

---
+++ Techie config.bat
@@ -0,0 +1,19 @@
+@ECHO OFF
+SET LOCALDIR=C:\WEB\WEBFILES\IYKEDAIRO\Techie
+CD %LOCALDIR%
+xcopy C:\Web\WEBFILES\config %LOCALDIR%\.git\config /G /Y
+
+cmdkey /delete:git:https//github.com
+git init
+git config --system --unset credential.helper
+git config credential.helper 'store file=C:\WEB\WEBFiles\IykeDairo\Plain\gits'
+git config --local user.name PrizeTechie
+git config --local  user.password iykesMan22
+git config --local user.email prizetechie@gmail.com
+git remote add Local https://github.com/PrizeTechie/CDN.git
+git remote set-url --add Local https://github.com/PrizeTechie/CDN.git
+git pull Local master --allow-unrelated-histories
+git remote -v
+
+PAUSE
+
---
+++ Techie push.bat
@@ -0,0 +1,4 @@
+SET COMMITMESSAGE="Techie Inc. Autos:- %Date%"
+git add --all :/
+git commit -m %COMMITMESSAGE% :/
+git push Local master
